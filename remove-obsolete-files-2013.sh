fb2891227b9ccc401f45a33dcbef87b360344692
Dan Kegel <dank@kegel.com>
2013-12-27 13:55:14 -0800

    Script to remove all known obsolete files from the old site

---
+++ remove-obsolete-files-2013.sh
@@ -0,0 +1,39 @@
+rm -rf  \
+aip_migrate_sermons.php \
+aip_parse_sermons.php \
+aip_set_page_aliases.php \
+aip_set_term_aliases.php \
+cgi-bin \
+chalice_lightings \
+chalice_lightings.zip \
+db \
+drupal7 \
+foo.make \
+.htaccess.bad \
+.htaccess.bak \
+.htaccess.bak2 \
+.htaccess.bak3 \
+log \
+newsletters.php \
+readdir.php \
+sermons \
+sermons-body.php \
+sermons.html \
+sermons_old.html \
+sermons.zip \
+sites/aliases.drushrc.php \
+sites/default/files/backup_migrate/scheduled/* \
+sites/default/files/css/* \
+sites/default/files/css.old \
+sites/default/files/imagecache/* \
+sites/default/files/imagefield_thumbs/* \
+sites/default/files/js/* \
+sites/default/files/js.old \
+sites/newsitedb \
+test.php \
+typescript \
+uuccsm \
+uuccsm.dir \
+xyzzy-apc.php \
+xyzzy-info.php
+
